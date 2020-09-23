e70274888f12a3a2174deff524ea5b28922f9073
MaengMaeng <maybe8189@naver.com>
2020-09-23 19:00:11 +0900

    Main Page 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import Main from './component/Main';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Main></Main>
       <Footer></Footer>
     </div>
   );
